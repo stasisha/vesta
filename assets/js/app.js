8b38a21d9cf715b5f209c299b48e9b0d02da537a
Stas <stas.bilov@gmail.com>
2017-11-23 05:19:16 +0200

    pgql fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,35 @@
-$(function(){
+$(function () {
     var animationTime = 100;
 
-    $('.advanced-install.link').click(function(){
+    $('.advanced-install.link').click(function () {
         $('#advanced-install').toggle();
     });
 
-    $('#fpm').change(function (e) {
-        if(this.val() == '--nginx yes --phpfpm yes --apache no'){
+    $('#web').change(function (e) {
+        if (this.val() == '--nginx yes --phpfpm yes --apache no') {
             $('#fpm').fadeOut(animationTime);
+            $('#fpm input').each(function (index) {
+                $(this).val('--phpfpm' + $(this).data('fpm') + ' yes');
+            });
         } else {
             $('#fpm').fadeIn(animationTime);
+            $('#fpm input').each(function (index) {
+                $(this).val('--phpfpm' + $(this).data('fpm') + ' no');
+            });
         }
     });
 
-    $('#install-form').submit(function(e){
+    $('#install-form').submit(function (e) {
         e.preventDefault();
         var install = 'bash vst-install.sh';
 
-        $('#install-form .form-control').each(function(i, elm){
-            if(elm.type == 'checkbox'){
-                if(elm.checked){
-                    install += ' '+$(elm).attr('option-name')+' yes'
-                } else {
-                    install += ' '+$(elm).attr('option-name')+' no'
+        $('#install-form .form-control').each(function (i, elm) {
+            if (elm.type == 'text') {
+                if (elm.value) {
+                    install += ' ' + elm.name + ' ' + elm.value;
                 }
-            } else if(elm.type == 'text'){
-                if(elm.value){
-                    install += ' '+elm.name+' '+elm.value;
-                }
-            } else if(elm.value) {
-                install += ' '+elm.value;
+            } else if (elm.value) {
+                install += ' ' + elm.value;
             }
         });
 
