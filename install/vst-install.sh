0316bad8cbe1274e04ed9a7978c054f9b6bfd443
teamcity <teamcity@teamcity.ml>
2017-12-07 09:41:42 +0200

    force fix

--- install/vst-install.sh
+++ install/vst-install.sh
@@ -19,26 +19,6 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 
-# Check admin user account
-if [ ! -z "$(grep ^admin: /etc/passwd)" ] && [ -z "$1" ]; then
-    echo "Error: user admin exists"
-    echo
-    echo 'Please remove admin user account before proceeding.'
-    echo 'If you want to do it automatically run installer with -f option:'
-    echo "Example: bash $0 --force"
-    exit 1
-fi
-
-# Check admin group
-if [ ! -z "$(grep ^admin: /etc/group)" ] && [ -z "$1" ]; then
-    echo "Error: group admin exists"
-    echo
-    echo 'Please remove admin group before proceeding.'
-    echo 'If you want to do it automatically run installer with -f option:'
-    echo "Example: bash $0 --force"
-    exit 1
-fi
-
 # Detect OS
 case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
     Debian)     type="debian" ;;
